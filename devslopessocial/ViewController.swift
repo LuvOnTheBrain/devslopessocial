1c09e271dfd3aab0c793f124f28026aa9d30c923
LuvOnTheBrain <lemonade@LemonadedeMacBook-Pro.local>
2016-11-15 20:05:47 +0800

    Facebook & Firebase Authentication completed

--- devslopessocial/ViewController.swift
+++ devslopessocial/ViewController.swift
@@ -9,33 +9,47 @@
 import UIKit
 import FBSDKLoginKit
 import FBSDKCoreKit
-
+import Firebase
 
 
 class ViewController: UIViewController {
-
-  
-    @IBOutlet weak var loginButton: FBSDKButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        loginButton = FBSDKLoginButton()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-    
     
     @IBAction func fbBtnTapped(_ sender: Any) {
-   
-       
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler: {(result, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("JESS: User cancelled Facebook authentication")
+            } else {
+                print("JESS: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+        )
+    }
     
+    // Code Source: https://developers.facebook.com/docs/swift/login
+    // first authenticated with provider(Facebook), then with Firebase
+    // After a user successfully signs in, get an access token for the signed-in user and exchange it for a Firebase credential:
+    // Finally, authenticate with Firebase using the Firebase credential:
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("JESS: Successfully authenticated with Firebase")
+            }
+        })
     }
 
-   
-    override func didReceiveMemoryWarning() {
+     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    
-
 }
 
