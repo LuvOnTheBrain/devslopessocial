46304987beb4ad605b749586997a4c6f07400fa6
LuvOnTheBrain <lemonade@229.38.176.60.broad.hz.zj.dynamic.163data.com.cn>
2016-11-15 12:12:24 +0800

    Completed Firebase setting

--- devslopessocial/ViewController.swift
+++ devslopessocial/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+
+
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var loginButton: FBSDKButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginButton = FBSDKLoginButton()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    @IBAction func fbBtnTapped(_ sender: Any) {
+      let login = FBSDKLoginManager.init()
+        login.logIn(withReadPermissions: @[@"public_profile"], from: self, handler: result: FBSDKLoginManagerLoginResult, error: NSError {
+            
+        })
+    }
 
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
