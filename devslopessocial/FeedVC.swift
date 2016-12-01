03bbe430899884889afdd6841069307d4885d5aa
LuvOnTheBrain <lemonade@LemonadedeMacBook-Pro.local>
2016-12-01 15:06:07 +0800

    Completed

--- devslopessocial/FeedVC.swift
+++ devslopessocial/FeedVC.swift
@@ -14,16 +14,28 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print(snapshot.value)
-            
-            
         
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
+      
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -31,10 +43,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("JESS:\(post.caption)")
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
     
@@ -43,7 +57,6 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         print("JESS: ID removed from keychain \(keychainResult)")
         try! FIRAuth.auth()?.signOut()
         performSegue(withIdentifier: "goToSignIn", sender: nil)
-        
     }
     
 }
